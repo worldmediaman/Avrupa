3ce602e8cc1179cb3328922da5295917eab9bc2b
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-17 19:51:12 +0100

    Delete Stream/cnn.sh

--- Stream/cnn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # Skript bei Fehlern abbrechen
-set -x  # Debugging aktivieren
-
-# Ausführung des Python-Skripts und Umleitung der Ausgabe
-python3 Stream/cnn.py > Stream/cnn.m3u8
-
-exit 0
