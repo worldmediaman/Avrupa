c673d50fa3ea780c13a9ba19718d12e7eeb3fec9
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-17 18:50:09 +0100

    Update cnn.sh

--- Stream/cnn.sh
+++ Stream/cnn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -e  # Skript bei Fehlern abbrechen
+set -x  # Debugging aktivieren
 
-python3  Stream/cnn.py > Stream/cnn.m3u8
+# Ausführung des Python-Skripts und Umleitung der Ausgabe
+python3 Stream/cnn.py > Stream/cnn.m3u8
 
 exit 0
