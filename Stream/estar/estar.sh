210ff9935a9f8bc2c4f21805bc3091bd985fbcae
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-17 19:12:35 +0100

    Update estar.sh

--- Stream/estar/estar.sh
+++ Stream/estar/estar.sh
@@ -2,6 +2,9 @@
 set -e  # Skript bei Fehlern abbrechen
 set -x  # Debugging aktivieren
 
+# Verzeichnisinhalt anzeigen
+ls -la Stream/estar
+
 # Ausführung des Python-Skripts und Umleitung der Ausgabe
 python3 Stream/estar/estar.py > Stream/estar/estar.m3u8
 
